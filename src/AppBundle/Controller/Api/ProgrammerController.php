a20035c2517962d24b58344e8ec2e66b79a2c05a
Neandher Carlos <neandher89@gmail.com>
2016-03-26 23:30:30 -0300

    Aula - PATCH is (also) for Updating (basically) - Finalizada

---
+++ src/AppBundle/Controller/Api/ProgrammerController.php
@@ -0,0 +1,183 @@
+<?php
+
+namespace AppBundle\Controller\Api;
+
+use AppBundle\Controller\BaseController;
+use AppBundle\Entity\Programmer;
+use AppBundle\Form\ProgrammerType;
+use AppBundle\Form\UpdateProgrammerType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ProgrammerController extends BaseController
+{
+
+    /**
+     * @Route("/api/programmers")
+     * @Method("POST")
+     * @param Request $request
+     * @return Response
+     */
+    public function newAction(Request $request)
+    {
+        $programmer = new Programmer();
+        $form = $this->createForm(new ProgrammerType(), $programmer);
+
+        $this->processForm($request, $form);
+
+        $programmer->setUser($this->findUserByUsername('weaverryan'));
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($programmer);
+        $em->flush();
+
+        $data = $this->serializeProgrammer($programmer);
+
+        $response = new JsonResponse($data, 201);
+
+        $programmerUrl = $this->generateUrl(
+            'api_programmers_show',
+            [
+                'nickname' => $programmer->getNickname()
+            ]
+        );
+
+        $response->headers->set('Location', $programmerUrl);
+
+        return $response;
+    }
+
+    /**
+     * @Route("/api/programmers/{nickname}", name="api_programmers_show")
+     * @Method("GET")
+     * @param $nickname
+     * @return Response
+     */
+    public function showAction($nickname)
+    {
+        $programmer = $this->getDoctrine()
+            ->getRepository('AppBundle:Programmer')
+            ->findOneByNickname($nickname);
+
+        if (!$programmer) {
+            throw $this->createNotFoundException(
+                sprintf(
+                    'No programmer with nickname "%s"',
+                    $nickname
+                )
+            );
+        }
+
+        $data = $this->serializeProgrammer($programmer);
+
+        $response = new JsonResponse($data, 200);
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
+    }
+
+    /**
+     * @Route("/api/programmers")
+     * @Method("GET")
+     */
+    public function listAction()
+    {
+        $programmers = $this->getDoctrine()
+            ->getRepository('AppBundle:Programmer')
+            ->findAll();
+
+        $data = array('programmers' => array());
+
+        foreach ($programmers as $programmer) {
+            $data['programmers'][] = $this->serializeProgrammer($programmer);
+        }
+
+        $response = new JsonResponse($data, 200);
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
+    }
+
+    private function serializeProgrammer(Programmer $programmer)
+    {
+        return array(
+            'nickname'     => $programmer->getNickname(),
+            'avatarNumber' => $programmer->getAvatarNumber(),
+            'powerLevel'   => $programmer->getPowerLevel(),
+            'tagLine'      => $programmer->getTagLine(),
+        );
+    }
+
+    /**
+     * @Route("/api/programmers/{nickname}")
+     * @Method({"PUT", "PATCH"})
+     * @param $nickname
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function updateAction($nickname, Request $request)
+    {
+        $programmer = $this->getDoctrine()
+            ->getRepository('AppBundle:Programmer')
+            ->findOneByNickname($nickname);
+
+        if (!$programmer) {
+            throw $this->createNotFoundException(sprintf(
+                'No programmer found with nickname "%s"',
+                $nickname
+            ));
+        }
+
+        $form = $this->createForm(new UpdateProgrammerType(), $programmer);
+
+        $this->processForm($request, $form);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($programmer);
+        $em->flush();
+
+        $data = $this->serializeProgrammer($programmer);
+
+        $response = new JsonResponse($data, 200);
+
+        return $response;
+    }
+
+    /**
+     * @Route("/api/programmers/{nickname}")
+     * @Method("DELETE")
+     * @param $nickname
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function deleteAction($nickname, Request $request)
+    {
+        $programmer = $this->getDoctrine()
+            ->getRepository('AppBundle:Programmer')
+            ->findOneByNickname($nickname);
+
+        if ($programmer) {
+            // debated point: should we 404 on an unknown nickname?
+            // or should we just return a nice 204 in all cases?
+            // we're doing the latter
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($programmer);
+            $em->flush();
+        }
+
+        return new Response(null, 204);
+    }
+
+    private function processForm(Request $request, FormInterface $form)
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $clearMissing = $request->getMethod() != 'PATCH';
+
+        $form->submit($data, $clearMissing);
+    }
+}
---
+++ src/AppBundle/Tests/Controller/Api/ProgrammerControllerTest.php
@@ -0,0 +1,181 @@
+<?php
+
+namespace AppBundle\Tests\Controller\Api;
+
+use AppBundle\Entity\Programmer;
+use AppBundle\Test\ApiTestCase;
+use Symfony\Component\PropertyAccess\PropertyAccess;
+
+class ProgrammerControllerTest extends ApiTestCase
+{
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->createUser('weaverryan');
+    }
+
+    public function testPost()
+    {
+
+        $nickname = 'ObjectOrienter' . rand(0, 999);
+
+        $data = array(
+            'nickname'     => $nickname,
+            'avatarNumber' => 5,
+            'tagLine'      => 'a test dev'
+        );
+
+        $response = $this->client->post(
+            '/api/programmers',
+            [
+                'body' => json_encode($data)
+            ]
+        );
+
+        $this->assertEquals(201, $response->getStatusCode());
+        $this->assertTrue($response->hasHeader('Location'));
+        $this->assertStringEndsWith('/api/programmers/' . $nickname, $response->getHeader('Location'));
+        $finishedData = json_decode($response->getBody(true), true);
+        $this->assertArrayHasKey('nickname', $finishedData);
+        $this->assertEquals($nickname, $finishedData['nickname']);
+    }
+
+    public function testGETProgrammer()
+    {
+        $this->createProgrammer(
+            array(
+                'nickname'     => 'UnitTester',
+                'avatarNumber' => 3,
+            )
+        );
+
+        $response = $this->client->get('/api/programmers/UnitTester');
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->asserter()->assertResponsePropertiesExist(
+            $response,
+            array(
+                'nickname',
+                'avatarNumber',
+                'powerLevel',
+                'tagLine'
+            )
+        );
+        $this->asserter()->assertResponsePropertyEquals($response, 'nickname', 'UnitTester');
+    }
+
+    public function testGETProgrammersCollection()
+    {
+        $this->createProgrammer(
+            array(
+                'nickname'     => 'UnitTester',
+                'avatarNumber' => 3,
+            )
+        );
+        $this->createProgrammer(
+            array(
+                'nickname'     => 'CowboyCoder',
+                'avatarNumber' => 5,
+            )
+        );
+
+        $response = $this->client->get('/api/programmers');
+        //$this->printLastRequestUrl();
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->asserter()->assertResponsePropertyIsArray($response, 'programmers');
+        //$this->asserter()->assertResponsePropertyCount($response, 'programmers', 7);
+        $this->asserter()->assertResponsePropertyEquals($response, 'programmers[1].nickname', 'CowboyCoder');
+    }
+
+    protected function createProgrammer(array $data)
+    {
+        $data = array_merge(
+            array(
+                'powerLevel' => rand(0, 10),
+                'user'       => $this->getEntityManager()->getRepository('AppBundle:User')->findAny()
+            ),
+            $data
+        );
+
+        // Use PropertyAccess component instead of iterating and calling each setter.
+        // Call $accessor->setValue(), pass in $programmer, $key (property name) and $value.
+        $accessor = PropertyAccess::createPropertyAccessor();
+
+        $programmer = new Programmer();
+
+        foreach ($data as $key => $value) {
+            $accessor->setValue($programmer, $key, $value);
+        }
+
+        $this->getEntityManager()->persist($programmer);
+        $this->getEntityManager()->flush();
+
+        return $programmer;
+    }
+
+    public function testPUTProgrammerController()
+    {
+        $this->createProgrammer(
+            array(
+                'nickname'     => 'CowboyCoder',
+                'avatarNumber' => 5,
+                'tagLine'      => 'foo',
+            )
+        );
+
+        $data = array(
+            'nickname'     => 'CowboyCoder',
+            'avatarNumber' => 2,
+            'tagLine'      => 'foo',
+        );
+
+        $response = $this->client->put(
+            '/api/programmers/CowboyCoder',
+            [
+                'body' => json_encode($data)
+            ]
+        );
+
+        $this->asserter()->assertResponsePropertyEquals($response, 'avatarNumber', 2);
+    }
+
+    public function testDELETEProgrammer($nickname, Request $request)
+    {
+        $this->createProgrammer(
+            array(
+                'nickname'     => 'UnitTester',
+                'avatarNumber' => 3,
+            )
+        );
+
+        $response = $this->client->delete('/api/programmers/UnitTester');
+        $this->assertEquals(204, $response->getStatusCode());
+    }
+
+    public function testPATCHProgrammer()
+    {
+        $this->createProgrammer(
+            array(
+                'nickname'     => 'CowboyCoder',
+                'avatarNumber' => 5,
+                'tagLine'      => 'foo',
+            )
+        );
+
+        $data = array(
+            'tagLine'      => 'bar',
+        );
+
+        $response = $this->client->patch(
+            '/api/programmers/CowboyCoder',
+            [
+                'body' => json_encode($data)
+            ]
+        );
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->asserter()->assertResponsePropertyEquals($response, 'avatarNumber', 5);
+        $this->asserter()->assertResponsePropertyEquals($response, 'tagLine', 'bar');
+    }
+
+}
